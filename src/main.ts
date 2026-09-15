3a7e0e40d0a4f0262fce8e70d90acbe39b08ff6b
VrecTangleRNG <vigoeka5@gmail.com>
2026-09-15 17:26:01 +0700

    Drawn player to screen

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
-import { Application } from "pixi.js";
+import { Application, Sprite, Assets } from "pixi.js";
+import { textureManager } from './systems/assets';
 
-const app = new Application();
+async function main(): Promise<void> {
+	const app = new Application();
 
-await app.init({
-    width: 960,
-    height: 540,
-    backgroundColor: 0x202020
-});
+	await app.init({
+		width: 960,
+		height: 540,
+		backgroundColor: 0x000000
+	});
 
-document.body.appendChild(app.canvas);
+	document.body.appendChild(app.canvas);
+
+	const textures = new textureManager();
+	await textures.load();
+
+	const player = Sprite.from("player");
+	app.stage.addChild(player);
+
+	const gun = Sprite.from("pistol");
+	app.stage.addChild(gun);
+
+	player.anchor.set(0.5);
+	player.position.x = player.width / 2;
+	player.position.y = app.screen.height - player.height / 2;
+
+	app.ticker.add((ticker) => {
+		let delta = ticker.deltaMS / 1000;
+	});
+}
+
+await main();
---
+++ src/systems/assets.ts
@@ -0,0 +1,15 @@
+import { Assets } from 'pixi.js';
+
+
+export class textureManager {
+	async load() {
+		await Assets.load([
+			{ alias: "player", src: "/assets/textures/char0.png" },
+			{ alias: "background", src: "/assets/textures/bg.png" },
+			{ alias: "particle", src: "/assets/textures/particle.png" },
+			{ alias: "pistol", src: "/assets/textures/glock.png" },
+		]);
+	}
+}
+
+//export class sounds {}
